65c7314ccba7ac0d6091746f20dd93bae21ed4f9
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:51:36 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -5,7 +5,6 @@ export default [
 { href: './html/Fabled Conquerors/index.html', imgSrc: 'fabled conquerors.jpeg', name: 'Fabled Conquerors' , 'page': 5, 'id': 811 },
 { href: './html/Escaping the prison/index.html', imgSrc: 'escaping the prison.jpeg', name: 'Escaping the prison' , 'page': 5, 'id': 810 },
 { href: './html/DOOM/index.html', imgSrc: 'doom.jpeg', name: 'DOOM' , 'page': 5, 'id': 795 },
-{ href: './html/Choppy Orc/index.html', imgSrc: 'choppy orc.jpeg', name: 'Choppy Orc' , 'page': 5, 'id': 790 },
 { href: './html/Case Clicker 2/index.html', imgSrc: 'case clicker 2.jpeg', name: 'Case Clicker 2' , 'page': 5, 'id': 780 },
 { href: './html/Capybara clicker/index.html', imgSrc: 'capybara clicker.jpeg', name: 'Capybara clicker' , 'page': 5, 'id': 777 },
 { href: './html/Burnin rubber/index.html', imgSrc: 'Burnin rubber.jpeg', name: 'Burnin rubber' , 'page': 5, 'id': 772 },
